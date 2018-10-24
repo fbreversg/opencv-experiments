56fceb1d77634475b427bb2b149e46552ddfb528
Paco Brevers <francisco.brevers@tecnilogica.com>
2018-10-24 10:53:11 +0200

    Operaciones de dibujado y overlay de texto.

--- pyImageSearch/openCV_Tutorial/opencv_tutorial_01.py
+++ pyImageSearch/openCV_Tutorial/opencv_tutorial_01.py
@@ -76,8 +76,28 @@ cv2.imshow("Blurred", blurred)
 cv2.waitKey(0)
 
 # draw a 2px thick red rectangle surrounding the face
+""" cv.Rectangle(img, pt1, pt2, color, thickness=1, lineType=8, shift=0) """
 output = image.copy()
 cv2.rectangle(output, (320, 60), (420, 160), (0, 0, 255), 2)
 cv2.imshow("Rectangle", output)
 cv2.waitKey(0)
 
+# draw a blue 20px (filled in) circle on the image centered at
+""" cv.Circle(img, center, radius, color, thickness=1, lineType=8, shift=0) """
+output = image.copy()
+cv2.circle(output, (300, 150), 20, (255, 0, 0), -1)
+cv2.imshow("Circle", output)
+cv2.waitKey(0)
+
+# draw a 5px thick red line from x=60,y=20 to x=400,y=200
+""" cv.Line(img, pt1, pt2, color, thickness=1, lineType=8, shift=0) """
+output = image.copy()
+cv2.line(output, (60, 20), (400, 200), (0, 0, 255), 5)
+cv2.imshow("Line", output)
+cv2.waitKey(0)
+
+# draw green text on the image
+output = image.copy()
+cv2.putText(output, "Texto", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+cv2.imshow("Text", output)
+cv2.waitKey(0)
